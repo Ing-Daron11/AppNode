42b2294cbc8f4b3c1cb8231702f5c0822c73a6ec
Ing-Daron11 <daronmercadog@gmail.com>
2025-03-25 22:35:07 -0500

    add dist fixed for vercel

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = handler;
 //let express = require("express");
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
@@ -14,21 +13,14 @@ const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
+//rutas para los endpoints
 app.use('/user', routes_1.userRouter);
 app.use('/computer', routes_1.computerRouter);
 app.use('/rental', routes_1.rentalRouter);
-app.get('/', (req, res) => {
-    res.send("Hello World");
-});
-app.get('/error', (req, res) => {
-    res.status(500).send("Hello World");
-});
-app.get('/notfound', (req, res) => {
-    res.status(404).send("Hello World");
+app.get("/", (req, res) => {
+    res.json({ message: "¡Servidor funcionando en Vercel!" });
 });
 connectionDB_1.db.then(() => app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 }));
-function handler(req, res) {
-    res.status(200).json({ message: "¡Servidor funcionando en Vercel!" });
-}
+exports.default = app;
